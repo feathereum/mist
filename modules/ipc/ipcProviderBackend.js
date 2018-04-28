7f06ef152d917be64c9eb2ad2adbce0f8afdb958
kyouminato <mcoinshien@gmail.com>
2018-04-28 17:11:32 +0900

    Icons and other ether -> feather

--- modules/ipc/ipcProviderBackend.js
+++ modules/ipc/ipcProviderBackend.js
@@ -161,7 +161,7 @@ class IpcProviderBackend {
                       ethereumNode.removeListener('state', onStateChange);
 
                       log.debug(
-                        `Ethereum node connected, resume connecting socket ${ownerId}`
+                        `Feathereum node connected, resume connecting socket ${ownerId}`
                       );
 
                       resolve();
@@ -225,7 +225,7 @@ class IpcProviderBackend {
     switch (state) { // eslint-disable-line default-case
       // stop syncing when node about to be stopped
       case ethereumNode.STATES.STOPPING:
-        log.info('Ethereum node stopping, disconnecting sockets');
+        log.info('Feathereum node stopping, disconnecting sockets');
 
         Q.all(
           _.map(this._connections, item => {
